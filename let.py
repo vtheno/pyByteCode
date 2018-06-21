f6472806524788eeac2f6237dd7bf96a07e47134
vtheno <a2550591@gmail.com>
2018-06-21 13:36:46 +0800

    init

---
+++ let.py
@@ -0,0 +1,91 @@
+#coding=utf-8
+import dis
+import opcode
+from types import CodeType
+from types import FunctionType
+
+def showCodeInfo(co):
+    print( " --------start------- ")
+    for i in dir(co):
+        if i.startswith('co'):
+            t = getattr(co,i)
+            print( '  ',repr(t),'\t: (',i,':',type(t).__name__,')' )
+    print( " ---------end--------- ")
+
+class let(object):
+    def __init__(self,env):
+        self.env = env
+    def findArgument(self,lst,target):
+        for i in range(len(lst)):
+            if lst[i] == target:
+                return lst[i+1]
+        return None
+    def replaceBinlst(self,lst,source,target,value):
+        newlst = [ ]
+        for i in range(len(lst)):
+            if lst[i] == source:
+                newlst += [target]
+                lst[i+1] = value
+            else:
+                newlst += [lst[i]]
+        return newlst
+    def genCode(self,old_const,old_varnames):
+        length_c = len(list(old_const))
+        length_v = len(list(old_varnames))
+        code = [ ]
+        keys = list(self.env.keys())
+        for i in range(len(keys)):
+            code += [opcode.opmap["LOAD_CONST"],length_c + i,
+                     opcode.opmap["STORE_FAST"],length_v + i]
+        #print( "genCode:",code )
+        return code
+    def processGlobals(self,lst,names,varnames):
+        #print( lst )            # LOAD_GLOBAL => 116
+        #print( names )
+        t = self.findArgument(lst,opcode.opmap["LOAD_GLOBAL"])
+        #print( t )
+        if t and self.env.get(names[t]) :
+            return self.replaceBinlst(lst,opcode.opmap["LOAD_GLOBAL"],opcode.opmap["LOAD_FAST"]
+                                      ,varnames.index(names[t]))
+        return lst
+    def __call__(self,func):
+        self.func = func
+        _globals_ = func.__globals__
+        _code_ = func.__code__
+        varnames  = list(_code_.co_varnames) + [v for v in self.env.keys()]
+        nlocals   = len(varnames)
+        binlst = list(_code_.co_code)
+        lstbin = self.processGlobals(binlst,_code_.co_names,varnames)
+        genCo = self.genCode(_code_.co_consts,_code_.co_varnames)
+        lstbin = genCo + lstbin
+        consts = tuple( list(_code_.co_consts) + [v for v in self.env.values()] )
+        code = CodeType(_code_.co_argcount,       # argcount
+                        _code_.co_kwonlyargcount, # kwonlyargcount
+                        nlocals,                  # nlocals
+                        _code_.co_stacksize,      # stacksize
+                        _code_.co_flags,          # flags
+                        bytes(lstbin),            # codestring
+                        consts,                   # consts
+                        _code_.co_names,          # names
+                        tuple(varnames),                 # varnames
+                        _code_.co_filename,       # filename
+                        _code_.co_name,           # name
+                        _code_.co_firstlineno,    # firstlineno
+                        _code_.co_lnotab,         # lnotab
+                        _code_.co_freevars,       # freevars
+                        _code_.co_cellvars,       # cellvars
+                    )
+        _globals_.update(self.env)
+        function = FunctionType(code,_globals_)
+        return function
+    def __repr__(self):
+        return "Where ({}) => {}".format(self.env,self.func)
+
+@let({'a':666})
+def test():
+    return a
+
+dis.dis(test)
+#showCodeInfo(test.__code__)
+print( test )
+print( test() )
---
+++ mycode.py
@@ -0,0 +1,143 @@
+#coding=utf-8
+import dis
+import opcode
+from types import CodeType
+"""
+CodeType(
+        argcount,             #   integer
+        kwonlyargcount,       #   integer
+        nlocals,              #   integer
+        stacksize,            #   integer
+        flags,                #   integer
+        codestring,           #   bytes
+        consts,               #   tuple
+        names,                #   tuple
+        varnames,             #   tuple
+        filename,             #   string
+        name,                 #   string
+        firstlineno,          #   integer
+        lnotab,               #   bytes
+        freevars,             #   tuple
+        cellvars              #   tuple
+        )
+"""
+
+def code():
+    return a + 1
+
+dis.dis(code.__code__)
+co_code = code.__code__.co_code
+print( co_code,type(co_code) )
+l1 = [i for i in co_code]
+print( l1 )
+l2 = [124] + [i for i in l1[1:]]
+co_code2 = bytes(l2)
+print( l2 ,co_code2 )
+
+
+code = 'x + 2'
+a = compile(code,'<string>','eval')
+def showCodeInfo(co):
+    print( " --------start------- ")
+    for i in dir(co):
+        if i.startswith('co'):
+            t = getattr(co,i)
+            print( '  ',repr(t),'\t: (',i,':',type(t).__name__,')' )
+    print( " ---------end--------- ")
+dis.dis(a)
+bincode = a.co_code
+
+print( a.co_names,a.co_consts )
+lstbin = [i for i in bincode]
+print( bincode,lstbin )
+print( opcode.opmap['LOAD_CONST'] )
+print( opcode.opmap['STORE_NAME'] )
+
+
+def f(x):
+    return x
+    
+dis.dis(f)
+
+m = CodeType(0,            # argcount
+             0,            # kwonlyargcount
+             0,            # nlocals
+             2,            # stacksize
+             64,           # flags
+             bytes(lstbin),# codestring
+             (66,),        # consts
+             ('x',),       # names
+             tuple(),      # varnames
+             '<string>',   # filename
+             'm',    # name
+             1,            # firstlineno
+             b'',          # lnotab
+             tuple(),      # freevars
+             tuple(),      # cellvars
+)
+print( m )
+x = 0
+print( eval(m) )
+print( m )
+d = compile('a = 2','<string>','exec')
+showCodeInfo(d)
+dis.dis(d)
+print( list(d.co_code) )
+lstbin = [opcode.opmap["LOAD_CONST"],0,
+          opcode.opmap["STORE_NAME"],0,
+          opcode.opmap["LOAD_NAME"],0,
+          opcode.opmap["RETURN_VALUE"],0]
+print(lstbin)
+c = CodeType(0,            # argcount
+             0,            # kwonlyargcount
+             0,            # nlocals
+             2,            # stacksize
+             64,           # flags
+             bytes(lstbin),# codestring
+             (66,None),        # consts
+             ('x',),       # names
+             tuple(),      # varnames
+             '<string>',   # filename
+             'm',    # name
+             1,            # firstlineno
+             b'',          # lnotab
+             tuple(),      # freevars
+             tuple(),      # cellvars
+)
+print( eval(c) )
+showCodeInfo(c)
+# co_varnames ( first argument , other local name
+def setf(variable,value):
+    temp = value
+    variable = temp
+    return variable
+dis.dis(setf)
+showCodeInfo(setf.__code__)
+lstbin = [opcode.opmap["LOAD_FAST"],1,
+          opcode.opmap["STORE_FAST"],0,
+          opcode.opmap["LOAD_FAST"],0,
+          opcode.opmap["RETURN_VALUE"],0]
+
+s = CodeType(2,            # argcount
+             0,            # kwonlyargcount
+             2,            # nlocals
+             1,            # stacksize
+             67,           # flags
+             bytes(lstbin),# codestring
+             (None,),        # consts
+             tuple(),       # names
+             ('variable','value'),      # varnames
+             '<string>',   # filename
+             'setv',    # name
+             1,            # firstlineno
+             b'',          # lnotab
+             tuple(),      # freevars
+             tuple(),      # cellvars
+)
+from types import FunctionType
+ss = FunctionType(s,# code
+                  globals(),# globals
+              )
+print( "----------" )
+print( ss(a,1) )
+
